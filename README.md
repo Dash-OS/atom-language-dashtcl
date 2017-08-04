a13631e2259e5813dedb17b134241178abd93580
Braden Napier <brady.napier@dashos.net>
2017-08-04 16:46:43 -0700

    add screenshot

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# language-dashtcl package for atom.io
+# Tcl 8.6.6 Enhanced Grammars
 
 This is an enhanced grammar adding an extensive amount of command-specific
 highlighting for Tcl.  This is far from completed and may exhibit odd
 behavior at certain points but many of the core commands are handled
 properly.
+
+This is currently a work in progress and is missing some core handling
+and commands.  We are currently adding and building to it as we encounter
+issues with the highlighting while moving our projects to atom editor.
+
+![image](http://i.imgur.com/3OhhAnb.png)
--- test.tcl
+++ test.tcl
@@ -1,57 +1,25 @@
-package require ensembled
-namespace eval redelay ensembled
-
-::oo::class create ::redelay::mixin {
-
-  constructor args {
-    my variable __redelay_count
-    set __redelay_count 0
-    if { [self next] ne {} } {
-      next {*}$args
-    }
-  }
-
-  method redelay { {arg {}} } {
-    if { $arg eq "reset" } {
-      return [ my __redelay_reset ]
-    } else {
-      return [ my __redelay_tick {*}$arg ]
-    }
-  }
-  method __redelay_reset {} {
-    my variable __redelay_count
-    set __redelay_count 0
-  }
-  method redelay_max max {
-    my variable __redelay_max
-    set __redelay_max $max
-  }
-  method __redelay_tick { {max {}} } {
-    my variable __redelay_count
-    my variable __redelay_max
-    if { [info exists __redelay_max] && $max eq {} } {
-      set max [set __redelay_max]
-    } elseif { $max eq {} } {
-      set max 30
-    }
-    set interval [expr { min( $max,(pow(2,[incr __redelay_count])-1)) * 1000 }]
-    set delay    [expr { int( rand() * ( $interval - 1 + 1 ) + 1 ) + 1000 }]
-    return $delay
-  }
+if 0 {
+  @ extend
+    > Credits
+      | Contributed by PYK
+    > References
+      | http://wiki.tcl.tk/28961?redir=28962
+      | http://wiki.tcl.tk/15566
+}
+proc extend {ens script} {
+  uplevel 1 [string map [list %ens [list $ens]] {
+    namespace ensemble configure %ens -unknown [list ::apply [list {ens cmd args} {
+      ::if {$cmd in [::namespace eval ::${ens} {::info commands}]} {
+        ::set map [::namespace ensemble configure $ens -map]
+        ::dict set map $cmd ::${ens}::$cmd
+        ::namespace ensemble configure $ens -map $map
+      }
+      ::return {} ; # back to namespace ensemble dispatch
+                  ; # which will error appropriately if the cmd doesn't exist
+    } [namespace current]]]
+  }]\;[list namespace eval $ens $script]
 }
 
-proc ::redelay::redelay { { arg {} } } {
-  variable controller
-  if { ! [info exists controller] } {
-    set controller [ mixin new ]
-  }
-  return [$controller redelay $arg]
-}
-
-proc ::redelay::new { {max {}} } {
-  set controller [mixin new]
-  if { $max ne {} } {
-    $controller redelay_max $max
-  }
-  return $controller
-}
+apply [list {ens cmd args} {
+  puts $ens 
+} $one $two]
