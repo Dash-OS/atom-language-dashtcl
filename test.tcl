4806c451c4a2fa973b98ca2be24bd179ceb2f613
Braden Napier <brady.napier@dashos.net>
2017-08-04 16:06:38 -0700

    updates to grammars

--- test.tcl
+++ test.tcl
@@ -1,12 +1,57 @@
-proc ::utils::vendor {} {
-  if { [info exists ::env(VENDOR)] } { return $::env(VENDOR) }
-  if { [ file exists [ file join / remote Store App BS.exe ] ] } {
-    return [ set ::env(VENDOR) "URC" ]
-  } elseif { [ file exists [ file join / control4 ] ] } {
-    return [ set ::env(VENDOR) "C4" ]
-  } else {
-    return [ set ::env(VENDOR) "UNKNOWN" ]
+package require ensembled
+namespace eval redelay ensembled
+
+::oo::class create ::redelay::mixin {
+
+  constructor args {
+    my variable __redelay_count
+    set __redelay_count 0
+    if { [self next] ne {} } {
+      next {*}$args
+    }
+  }
+
+  method redelay { {arg {}} } {
+    if { $arg eq "reset" } {
+      return [ my __redelay_reset ]
+    } else {
+      return [ my __redelay_tick {*}$arg ]
+    }
+  }
+  method __redelay_reset {} {
+    my variable __redelay_count
+    set __redelay_count 0
+  }
+  method redelay_max max {
+    my variable __redelay_max
+    set __redelay_max $max
+  }
+  method __redelay_tick { {max {}} } {
+    my variable __redelay_count
+    my variable __redelay_max
+    if { [info exists __redelay_max] && $max eq {} } {
+      set max [set __redelay_max]
+    } elseif { $max eq {} } {
+      set max 30
+    }
+    set interval [expr { min( $max,(pow(2,[incr __redelay_count])-1)) * 1000 }]
+    set delay    [expr { int( rand() * ( $interval - 1 + 1 ) + 1 ) + 1000 }]
+    return $delay
   }
 }
 
-incr ::one::two(three) 3
+proc ::redelay::redelay { { arg {} } } {
+  variable controller
+  if { ! [info exists controller] } {
+    set controller [ mixin new ]
+  }
+  return [$controller redelay $arg]
+}
+
+proc ::redelay::new { {max {}} } {
+  set controller [mixin new]
+  if { $max ne {} } {
+    $controller redelay_max $max
+  }
+  return $controller
+}
