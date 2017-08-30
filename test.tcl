3f652948cba88376d5696b49ddd92ee639664735
Braden Napier <brady.napier@dashos.net>
2017-08-29 18:13:18 -0700

    Initial support for Regular Expressions (regexp/regsub) with special highlighting

--- test.tcl
+++ test.tcl
@@ -1,8 +1,14 @@
 
 
-::vwait one
+regexp -all -inline {(?=[^\n]*\S)[[:alpha:]].*\.tcl[^\n]+?} $data -> one two three
+test
+
+regexp  {^([-+]?[0-9]+)([0-9][0-9][0-9])}
+
+regexp {^([-+]?\d+)(\d{3})$}
 
 
-set test [vwait forever]
-vwait forever tow
-puts one
+regexp  {<name>(.+)</name>(?:.*?<scope>(SYSTEM|PUBLIC)</scope>.*?<S_URI>(.+)</S_URI>(?:.*?<P_URI>(.+)</P_URI>)?)?(?:.*?<definition>(.*?)</definition>)?(?:.*?<attributes>(.*?)</attributes>)?.*?<content>(.*)</content>\s*$}
+
+
+regexp {^(https?:\/\/wiki.[[:alpha:]] [[:digit:]]\/)(?:.*\?redir=([0-9]+)|_\/edit\?N=([0-9]+)|([0-9]+))$}
